08ec52cf55bfc162fc9f261f7abbdacfc472fd2f
Andreas Fehn <fehnomenal@fehn.systems>
2025-01-11 16:50:37 +0100

    fix: wrap footer on mobile

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="myContainer flex-grow flex flex-row justify-between items-center text-gray-500">
-    <div class="flex flex-row gap-5">
+  <div class="myContainer flex-grow flex flex-col md:flex-row justify-between items-center text-gray-500">
+    <div class="flex flex-row flex-wrap gap-x-5 gap-y-2">
       <p>&copy; {{ new Date().getFullYear() }} The authors</p>
 
       <p>
