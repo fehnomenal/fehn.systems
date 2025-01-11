4fb959ba06a0a54f64f011dca7df37f83747baa0
Andreas Fehn <fehnomenal@fehn.systems>
2025-01-11 17:36:10 +0100

    hotfix: enable profile pic

--- components/bio/Image.vue
+++ components/bio/Image.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="w-3/4 aspect-square mx-auto flex flex-col content-center justify-center">
-    <NuxtImg
-      src="/me.jpg" alt="Profile picture" class="rounded-full" placeholder placeholder-class="object-fill"
-      sizes="300px md:260px lg:550px"
-    />
+    <img src="/me.jpg" alt="Profile picture" class="rounded-full" sizes="300px md:260px lg:550px">
   </div>
 </template>
