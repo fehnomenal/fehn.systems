02a55f7b47af6fa2f991b1b028ff304661649d29
Thorsten Seyschab <business@todde.tv>
2025-01-11 13:24:03 +0100

    refactor: tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#6366F1',
+          50: '#FFFFFF',
+          100: '#F9F9FE',
+          200: '#D3D4FB',
+          300: '#AEAFF8',
+          400: '#888BF4',
+          500: '#6366F1',
+          600: '#3034EC',
+          700: '#1317D1',
+          800: '#0E119E',
+          900: '#0A0C6A',
+          950: '#070950',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
