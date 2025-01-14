70d17a098a83b2370a034c8a3d707562b6720fb1
Andreas Fehn <fehnomenal@fehn.systems>
2025-01-14 10:19:13 +0100

    fix: fix vue warning about reactive component (#12)

--- components/ProjectModal.vue
+++ components/ProjectModal.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import type { DefineComponent } from 'vue'
 import { useVfm, VueFinalModal } from 'vue-final-modal'
 
 defineProps<{
-  project: DefineComponent
+  projectComponent: Component
 }>()
 
 const { close } = useVfm()
@@ -14,16 +13,19 @@ function closeModal() {
 </script>
 
 <template>
-  <VueFinalModal
-    class="flex justify-center items-center"
-    modal-id="project-modal"
-  >
-    <div class="w-screen p-4 h-screen flex justify-center items-center mx-auto max-w-7xl px-6 lg:px-8" @click="closeModal()">
-      <div class="flex flex-col p-4 bg-white border rounded-lg w-full max-h-full overflow-y-auto" @click.stop="() => {}">
+  <VueFinalModal class="flex justify-center items-center" modal-id="project-modal">
+    <div
+      class="w-screen p-4 h-screen flex justify-center items-center mx-auto max-w-7xl px-6 lg:px-8"
+      @click="closeModal()"
+    >
+      <div
+        class="flex flex-col p-4 bg-white border rounded-lg w-full max-h-full overflow-y-auto"
+        @click.stop="() => { }"
+      >
         <button class="mt-1 ml-auto px-2 border rounded-lg" @click="closeModal()">
           close
         </button>
-        <component :is="project" />
+        <component :is="projectComponent" />
       </div>
     </div>
   </VueFinalModal>
